713fc54ddac3c7f9da6d9d9a9278859fc11953ff
alvarosantos <asantosq1@gmail.com>
2023-07-13 18:01:05 +0200

    Create project structure and dependencies

---
+++ src/test/java/com/positiveref/inditex/InditexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.positiveref.inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
