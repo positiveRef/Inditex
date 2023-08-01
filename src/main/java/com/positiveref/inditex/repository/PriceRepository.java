a70ed5687f0d1338549e1d624ad3880ee1b316a9
alvarosantos <asantosq1@gmail.com>
2023-08-01 22:58:16 +0200

    Fix PriceRepository to match with previous refactor.

--- src/main/java/com/positiveref/inditex/repository/PriceRepository.java
+++ src/main/java/com/positiveref/inditex/repository/PriceRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
 
-    @Query(value = "SELECT p FROM PricesEntity as p " +
+    @Query(value = "SELECT p FROM PriceEntity as p " +
             "where p.startDate <= ?1 and p.endDate >= ?1 and p.productId = ?2 and p.brandId = ?3 order by p.priority desc")
     List<PriceEntity> findByDateAndProductIdAndBrandId(LocalDateTime dateTime, Long productId, Long brandId);
 }
--- src/test/java/com/positiveref/inditex/service/PriceServiceTest.java
+++ src/test/java/com/positiveref/inditex/service/PriceServiceTest.java
@@ -18,7 +18,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class PriceServiceImplTest {
+class PriceServiceTest {
 
     @InjectMocks
     private PriceServiceImpl pricesService;
